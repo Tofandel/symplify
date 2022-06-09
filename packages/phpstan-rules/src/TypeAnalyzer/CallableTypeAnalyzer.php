387b841a216dc811f03f344ed9b96391dc02bd30
GitHub Action <action@github.com>
2022-06-09 11:53:04 +0000

    [ci-review] Rector Rectify

--- packages/phpstan-rules/src/TypeAnalyzer/CallableTypeAnalyzer.php
+++ packages/phpstan-rules/src/TypeAnalyzer/CallableTypeAnalyzer.php
@@ -28,12 +28,13 @@ final class CallableTypeAnalyzer
         if ($unwrappedNameStaticType instanceof ClosureType) {
             return true;
         }
-
-        if ($unwrappedNameStaticType instanceof ObjectType && $unwrappedNameStaticType->getClassName() === Closure::class) {
-            return true;
+        if (!$unwrappedNameStaticType instanceof ObjectType) {
+            return $this->isInvokableObjectType($unwrappedNameStaticType);
         }
-
-        return $this->isInvokableObjectType($unwrappedNameStaticType);
+        if ($unwrappedNameStaticType->getClassName() !== Closure::class) {
+            return $this->isInvokableObjectType($unwrappedNameStaticType);
+        }
+        return true;
     }
 
     private function isInvokableObjectType(Type $type): bool
